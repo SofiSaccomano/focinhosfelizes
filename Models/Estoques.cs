fd269903b7ead67389dc4ab590d08930ee8a40b6
kawanydb <kawanydomingosbatista15@gmail.com>
2025-03-20 15:07:36 -0300

    Teste

--- Models/Estoques.cs
+++ Models/Estoques.cs
@@ -1,6 +1,23 @@
-﻿namespace FocinhosFelizes.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FocinhosFelizes.Models
 {
-    public class Estoque
+    [Table("Estoques")]
+    public class Estoques
     {
+        [Column("Id")]
+        [Display(Name = "Cód. Produto")]
+        public int Id { get; set; }
+
+        [ForeignKey("ProdutosId")]
+        [Display(Name = "Produtos")]
+        public string ProdutosId { get; set; } = string.Empty;
+        public Produtos? Produtos { get; set; }
+
+        [Column("QtdEstoque")]
+        [Display(Name = "Qtd. Estoque")]
+        public int QtdEstoque { get; set; }
+
     }
 }
